652b6560acf7f3f3a6a52be6a844c956dd7b4bfb
Edyta <dudek002@hotmail.com>
2018-02-19 19:34:57 -0600

    working on function update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 var inquirer =require("inquirer");
 var mysql =require("mysql");
 
+var items_left;
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -17,19 +19,9 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   queryAllProducts();
+  updateProduct();
 });
 
-/*
-function readColleges() {
-  connection.query("SELECT name FROM colleges", function(err, res) {
-    if (err) throw err;
-
-    // Log all results of the SELECT statement
-    console.log(res);
-    connection.end();
-  });
-}
-*/
 
 function queryAllProducts() {
   connection.query("SELECT * FROM products", function(err, res) {
@@ -40,28 +32,89 @@ function queryAllProducts() {
   });
 }
 
+
+function updateProduct() {
+  console.log("Updating quantities...\n");
+  var query = connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        stock_quantity: 100
+      },
+      {
+        item_ID: 1
+      }
+    ],
+    function(err, res) {
+      console.log(" products updated!\n");
+      // Call deleteProduct AFTER the UPDATE completes
+      //deleteProduct();
+    }
+  );
+
+  // logs the actual query being run
+  //console.log(query.sql);
+}
+
+
 function IDSearch() {
   inquirer
     .prompt([{
       name: "ID",
       type: "input",
-      message: "What is the ID number you are looking for?\n"
+      message: "What is the ID number of the item you are looking for?\n"
     },{
       name: "number",
       type: "input",
       message: "How many items woud you like to buy?"
     }])
     .then(function(answer) {
-      var query = "SELECT item_ID FROM products WHERE ?";
+      //number_required: answer.number;
+
+      var query = "SELECT item_ID, stock_quantity, price FROM products WHERE ?";
       connection.query(query, { 
-      	item_ID: answer.ID,
-      	number: answer.number 
+      	item_ID: answer.ID 
       }, function(err, res) {
         for (var i = 0; i < res.length; i++) {
-          console.log("Position: " + res[i].item_ID);
+          items_left=res[i].stock_quantity;
+          var item_price = res[i].price;
+          console.log("We have " + items_left + " items left with ID = " + res[i].item_ID);
+        }
+        if (items_left>=answer.number) {
+          console.log("You will receive your " + answer.number + " items in the mail.\n");
+          console.log("Your total cost will be " + (answer.number * item_price))
+        }
+        else 
+        {
+          console.log("We are sorry but we have insufficient quantity of this product.")
         }
       });
-    });
+
+      var query = "UPDATE products SET ? WHERE ?";
+
+      var new_stock = items_left - answer.number;
+      console.log("1 " + new_stock +"2 " + items_left + "3 " + answer.number);
+      connection.query(query,
+      [
+        {
+          stock_quantity: new_stock
+        },
+        {
+          item_ID: answer.ID
+        }
+      ],
+      function(err, res) {
+        console.log("My products updated!\n");
+        // Call deleteProduct AFTER the UPDATE completes
+        //deleteProduct();
+      }
+    );
+    
+
+
+
+  });
 }
 
 IDSearch();
+
